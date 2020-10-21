5437e8390a9091b00d673eade179a9dbc39ec285
gladkill <gladkillmusic@gmail.com>
2020-10-21 15:52:10 -0700

    initial commit

---
+++ src/Routes.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {Route, Switch, Redirect} from 'react-router-dom';
+import Navigation from './Navigation.js';
+import About from './About.js';
+import Projects from './Projects.js';
+import Services from './Services.js';
+import Contact from './Contact.js';
+import Homepage from './Homepage.js';
+
+
+function Routes(){
+  return(
+    <>
+    <Navigation/>
+    <Switch>
+      <Route exact path="/about">
+        <About />
+      </Route>
+      <Route exact path="/projects">
+        <Projects />
+      </Route>
+      <Route exact path="/services">
+        <Services />
+      </Route>
+      <Route exact path="/about">
+        <About />
+      </Route>
+      <Route exact path="/contact">
+        <Contact />
+      </Route>
+      <Route exact path="/">
+        <Homepage />
+      </Route>
+      <Redirect to="/" />
+    </Switch>
+    </>
+  )
+}
+
+export default Routes
