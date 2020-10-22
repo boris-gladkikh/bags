b2eece15c1c097a45426001e8ad646a2e05ef010
gladkill <gladkillmusic@gmail.com>
2020-10-22 01:31:44 -0700

    completed form controls and working on form submission

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,39 +1,75 @@
-import React, {useState} from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
-
-function ContactForm(){
+function ContactForm() {
   const [formData, setFormData] = useState();
 
-  function handleChange(){}
-  function handleSubmit(){}
-  return(
-    <Form className="my-5 m-auto w-75"> 
+  function handleChange(evt) {
+    let { name, value } = evt.target;
+    setFormData((form) => ({
+      ...form,
+      [name]: value,
+    }));
+  }
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    console.log("form data", formData);
+  }
+  return (
+    <Form className="my-5 m-auto w-75" onSubmit={handleSubmit}>
       <Form.Group>
         <Form.Label htmlFor="name"></Form.Label>
-      <Form.Control type="text" id="name" name="name" placeholder="Name" />
+        <Form.Control
+          onChange={handleChange}
+          required
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Name"
+        />
       </Form.Group>
 
       <Form.Group>
         <Form.Label htmlFor="email"></Form.Label>
-      <Form.Control type="Email" id="email" name="email" placeholder="Email" />
+        <Form.Control
+          onChange={handleChange}
+          required
+          type="Email"
+          id="email"
+          name="email"
+          placeholder="Email"
+        />
       </Form.Group>
 
       <Form.Group>
         <Form.Label htmlFor="subject"></Form.Label>
-      <Form.Control type="text" id="subject" name="subject" placeholder="subject" />
+        <Form.Control
+          onChange={handleChange}
+          required
+          type="text"
+          id="subject"
+          name="subject"
+          placeholder="Subject"
+        />
       </Form.Group>
 
       <Form.Group>
-        <Form.Label htmlFor="name"></Form.Label>
-      <Form.Control rows="5" as="textarea" id="name" name="name" />
+        <Form.Label htmlFor="body"></Form.Label>
+        <Form.Control
+          onChange={handleChange}
+          required
+          rows="5"
+          as="textarea"
+          id="body"
+          name="body"
+          placeholder="Body"
+        />
       </Form.Group>
-      
-      <Button>Submit</Button>
-    </Form>
 
-  )
+      <Button type="submit">Submit</Button>
+    </Form>
+  );
 }
 
-export default ContactForm
+export default ContactForm;
