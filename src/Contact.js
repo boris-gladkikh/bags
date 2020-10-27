4147c8dcb63f70d37b9920683c0a9f28bd3cfeaa
gladkill <gladkillmusic@gmail.com>
2020-10-26 22:54:56 -0700

    updated font to noto on contact page

--- src/Contact.js
+++ src/Contact.js
@@ -12,10 +12,10 @@ function Contact() {
           <h1>REACH OUT ANYTIME.</h1>
         </div>
       </div>
-      <div className="contact-section my-5">
+      <div className="noto-font contact-section my-5">
         <h4>707-563-9435</h4>
         <h4><a href="mailto:info@bagraphicsolutions.com">info@bagraphicsolutions.com</a></h4>
-        <h6 className="mt-4">
+        <h6 className="noto-font mt-4">
           We are available by phone from 9AM-5PM PST. <br />
           For inquiries, pricing, and general questions please use the provided
           form.
