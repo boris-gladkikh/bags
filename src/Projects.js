5418cb60322fd62de3bbbae73e909bb8e72e13aa
gladkill <gladkillmusic@gmail.com>
2020-10-28 13:50:10 -0700

    fixed about padding issue on mobile, starting to replace project  gallery with css grid

--- src/Projects.js
+++ src/Projects.js
@@ -5,6 +5,8 @@ import Col from "react-bootstrap/Col";
 import ProjectCard from "./ProjectCard";
 import projects from "./projectList";
 
+//component projects gallery. utilizes css grid for display, and renders projectCard components
+
 function Projects() {
   let renderProjects = projects.map((project) => (
     <Col sm="12" md="6" lg="4" xl="3">
