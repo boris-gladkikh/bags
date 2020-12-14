abe0a1b2e2c72092f791338e0a3a0cbdd5623c80
gladkill <gladkillmusic@gmail.com>
2020-12-13 23:48:00 -0800

    WIP modal styling and position

--- src/ProjectModal.js
+++ src/ProjectModal.js
@@ -3,16 +3,17 @@ import "./ProjectModal.css";
 
 function ProjectModal({ project, setShowModal }) {
   return (
-    <div onClick={setShowModal} className="project-modal">
-      <img src={project.coverImg} alt={project.title}></img>
-      <div className="project-modal-text">
-        <h4>{project.title}</h4>
-        <p>Click to close</p>
-        <p>{project.description}</p>
-
+    <div id="project-modal-container">
+      <div onClick={setShowModal} className="project-modal">
+        <img src={project.coverImg} alt={project.title}></img>
+        <div className="project-modal-text">
+          <h4>{project.title}</h4>
+          <p>Click to close</p>
+          <p>{project.description}</p>
+        </div>
       </div>
     </div>
   );
 }
 
-export default ProjectModal
+export default ProjectModal;
