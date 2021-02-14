783463fb980633caa185a6089872d22a94b7508d
gladkill <gladkillmusic@gmail.com>
2021-02-14 15:10:15 -0800

    fixed no  unused vars

--- src/functions.js
+++ src/functions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import nodemailer from 'nodemailer';
+// import nodemailer from 'nodemailer';
 const headers =  {"Content-Type": "application/x-www-form-urlencoded" };
 
 const functions = () => {
