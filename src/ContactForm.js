49e7f049d9db2086e524fe10b86edfd37aaf4706
gladkill <gladkillmusic@gmail.com>
2021-02-14 17:49:12 -0800

    added submit  message and alert code

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,43 +1,46 @@
 import React, { useState, useEffect } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import {submitEmailData} from './functions';
+import Alert from "react-bootstrap/Alert";
+import { submitEmailData } from "./functions";
 
 const INITDATA = {
-  name:"",
-  email:"",
-  subject:"",
-  message:"",
-}
+  name: "",
+  email: "",
+  subject: "",
+  message: "",
+};
 function ContactForm() {
-  const [formData, setFormData] = useState({...INITDATA});
+  const [formData, setFormData] = useState({ ...INITDATA });
   const [message, setMessage] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [alertVariant, setAlertVariant] = useState("");
 
-  useEffect(function(){
-    async function sendData() {
-      try{
-        const resp = await submitEmailData(formData);
-        setMessage("Succesfully Submitted");
-        console.log(resp)
-
-      } catch(err) {
-        console.error(err);
-        setMessage(err.message)
-      } 
-    }
-    if(isLoading){
-      sendData(formData);
-      setIsLoading(false);
-      setTimeout(function(){
-        setFormData({...INITDATA});
-        setMessage("");
-      }, 5000)
-    }
-  }, [formData, isLoading]);
-
-
-
+  useEffect(
+    function () {
+      async function sendData() {
+        try {
+          const resp = await submitEmailData(formData);
+          setAlertVariant("success");
+          setMessage("Succesfully Submitted");
+          console.log(resp);
+        } catch (err) {
+          console.error(err);
+          setMessage(err.message);
+          setAlertVariant("danger");
+        }
+      }
+      if (isLoading) {
+        sendData(formData);
+        setIsLoading(false);
+        setTimeout(function () {
+          setFormData({ ...INITDATA });
+          setMessage("");
+        }, 5000);
+      }
+    },
+    [formData, isLoading]
+  );
 
   function handleChange(evt) {
     let { name, value } = evt.target;
@@ -50,9 +53,21 @@ function ContactForm() {
     evt.preventDefault();
     setIsLoading(true);
   }
-  return (
-    <Form  name="contact-form" className="my-5 m-auto w-75" onSubmit={handleSubmit} >
 
+  let submitMessage =
+    message === "" ? (
+      ""
+    ) : (
+      <Alert className="mt-2" variant={alertVariant}>
+        <Alert.Heading className="text-center">{message}</Alert.Heading>
+      </Alert>
+    );
+  return (
+    <Form
+      name="contact-form"
+      className="my-5 m-auto w-75"
+      onSubmit={handleSubmit}
+    >
       <Form.Group>
         <Form.Label htmlFor="name"></Form.Label>
         <Form.Control
@@ -109,9 +124,7 @@ function ContactForm() {
       <Button variant="secondary" type="submit">
         Submit
       </Button>
-      <div>
-        {message}
-      </div>
+      {submitMessage}
     </Form>
   );
 }
